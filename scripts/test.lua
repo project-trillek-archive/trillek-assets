6fc37424276ec5dbad44be242763d1997d5dacb6
Adam Martin <adam.madram@gmail.com>
2014-08-17 19:23:06 -0500

    Moved camera code to Lua in its own file

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,17 +1,6 @@
-transform = Transform.Get(1000)
+sys = LuaSys.Get()
 
-scale = transform:get_scale()
-
-scale_direction = 1
+sys:LoadScriptFile("assets/scripts/sixdofcamera.lua")
 
 function Update(delta)
-    if (scale.x > 10) then
-        scale_direction = -1
-    elseif (scale.x < 0000) then
-        scale_direction = 1
-    end
-    scale.x = scale.x + 0.1 * scale_direction
-    scale.y = scale.y + 0.1 * scale_direction
-    scale.z = scale.z + 0.1 * scale_direction
-    transform:set_scale(scale.x, scale.y, scale.z)
 end
