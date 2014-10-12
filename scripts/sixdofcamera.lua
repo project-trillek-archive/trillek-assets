f2379f6d76d33e5fcf4d52c8643b20ba4e5f2657
Adam Martin <adam.madram@gmail.com>
2014-10-12 14:40:03 -0500

    Updated script to using new functions names
    The movement isn't oriented and the linear needs to be
    multiplied by the orientation.

--- scripts/sixdofcamera.lua
+++ scripts/sixdofcamera.lua
@@ -5,33 +5,32 @@ LuaSys.Get():Subscribe(5000, "MoveCamera")
 
 phys = Physics.Get()
 
-cameraLinear = {x = 0.0, y = 0.0, z = 0.0}
-cameraAngular = {x = 0.0, y = 0.0, z = 0.0}
+cameraVelocity = {x = 0.0, y = 0.0, z = 0.0, rx = 0.0, ry = 0.0, rz = 0.0}
 
 gravityEnabled = true;
 
 function MoveCamera(action, key)
     if (action == "Down") then
         if (key == string.byte("W")) then
-            cameraLinear.z = cameraLinear.z - 10.0
+            cameraVelocity.z = cameraVelocity.z - 10.0
         elseif (key == string.byte("S")) then
-            cameraLinear.z = cameraLinear.z + 10.0
+            cameraVelocity.z = cameraVelocity.z + 10.0
         elseif (key == string.byte("A")) then
-            cameraLinear.x = cameraLinear.x - 10.0
+            cameraVelocity.x = cameraVelocity.x - 10.0
         elseif (key == string.byte("D")) then
-            cameraLinear.x = cameraLinear.x + 10.0
+            cameraVelocity.x = cameraVelocity.x + 10.0
         elseif (key == 328) then
-            cameraAngular.x = cameraAngular.x + 5.0
+            cameraVelocity.rx = cameraVelocity.rx + 5.0
         elseif (key == 322) then
-            cameraAngular.x = cameraAngular.x - 5.0
+            cameraVelocity.rx = cameraVelocity.rx - 5.0
         elseif (key == 324) then
-            cameraAngular.y = cameraAngular.y + 5.0
+            cameraVelocity.ry = cameraVelocity.ry + 5.0
         elseif (key == 326) then
-            cameraAngular.y = cameraAngular.y - 5.0
+            cameraVelocity.ry = cameraVelocity.ry - 5.0
         elseif (key == 327) then
-            cameraAngular.z = cameraAngular.z + 5.0
+            cameraVelocity.rz = cameraVelocity.rz + 5.0
         elseif (key == 329) then
-            cameraAngular.z = cameraAngular.z - 5.0
+            cameraVelocity.rz = cameraVelocity.rz - 5.0
         elseif (key == 320) then
             if (gravityEnabled) then
                 gravityEnabled = false
@@ -43,30 +42,27 @@ function MoveCamera(action, key)
         end
     elseif (action == "Up") then
         if (key == string.byte("W")) then
-            cameraLinear.z = cameraLinear.z + 10.0
+            cameraVelocity.z = cameraVelocity.z + 10.0
         elseif (key == string.byte("S")) then
-            cameraLinear.z = cameraLinear.z - 10.0
+            cameraVelocity.z = cameraVelocity.z - 10.0
         elseif (key == string.byte("A")) then
-            cameraLinear.x = cameraLinear.x + 10.0
+            cameraVelocity.x = cameraVelocity.x + 10.0
         elseif (key == string.byte("D")) then
-            cameraLinear.x = cameraLinear.x - 10.0
+            cameraVelocity.x = cameraVelocity.x - 10.0
         elseif (key == 328) then
-            cameraAngular.x = cameraAngular.x - 5.0
+            cameraVelocity.rx = cameraVelocity.rx - 5.0
         elseif (key == 322) then
-            cameraAngular.x = cameraAngular.x + 5.0
+            cameraVelocity.rx = cameraVelocity.rx + 5.0
         elseif (key == 324) then
-            cameraAngular.y = cameraAngular.y - 5.0
+            cameraVelocity.ry = cameraVelocity.ry - 5.0
         elseif (key == 326) then
-            cameraAngular.y = cameraAngular.y + 5.0
+            cameraVelocity.ry = cameraVelocity.ry + 5.0
         elseif (key == 327) then
-            cameraAngular.z = cameraAngular.z - 5.0
+            cameraVelocity.rz = cameraVelocity.rz - 5.0
         elseif (key == 329) then
-            cameraAngular.z = cameraAngular.z + 5.0
+            cameraVelocity.rz = cameraVelocity.rz + 5.0
         elseif (key == 320) then
         end
     end
-    linear = camera:compute_velocity_vector(cameraLinear)
-    angular = camera:compute_velocity_vector(cameraAngular)
-    phys:set_force(10, linear)
-    phys:set_torque(10, angular)
+    phys:set_velocity(10, cameraVelocity)
 end
