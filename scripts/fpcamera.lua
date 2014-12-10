f8511cfbb670f99690484afe63d2711193f5b4d0
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2014-12-10 07:51:48 +0000

    Force based movement

--- scripts/fpcamera.lua
+++ scripts/fpcamera.lua
@@ -9,34 +9,38 @@ phys = Physics.Get()
 cameraVelocity = {x = 0.0, y = 0.0, z = 0.0, rx = 0.0, ry = 0.0, rz = 0.0}
 cameraR = {x = 0.0, y = 0.0, z = 0.0}
 gravityEnabled = true
+editit = false
 function MouseBFPS(state, btn)
 	if (state == "Down") and (btn == "Right") then
 		theos:ToggleMouseLock()
+	elseif (state == "Down") and (btn == "Left") then
+		editit = not editit
+		phys:set_movable(1004, editit)
 	end
 end
 
 function MoveCamera(action, key)
-	if (action == "Down") then
+    if (action == "Down") then
         if (key == 87) then
-            cameraVelocity.z = cameraVelocity.z - 10.0
+            cameraVelocity.z = -0.1
         elseif (key == string.byte("S")) then
-            cameraVelocity.z = cameraVelocity.z + 10.0
+            cameraVelocity.z = 0.1
         elseif (key == 65) then
-            cameraVelocity.x = cameraVelocity.x - 10.0
+            cameraVelocity.x = -0.1
         elseif (key == string.byte("D")) then
-            cameraVelocity.x = cameraVelocity.x + 10.0
+            cameraVelocity.x = 0.1
         elseif (key == 328) then
-            cameraVelocity.rx = cameraVelocity.rx + 5.0
+            cameraVelocity.rx = 5.0
         elseif (key == 322) then
-            cameraVelocity.rx = cameraVelocity.rx - 5.0
+            cameraVelocity.rx = -5.0
         elseif (key == 324) then
-            cameraVelocity.ry = cameraVelocity.ry + 5.0
+            cameraVelocity.ry = 5.0
         elseif (key == 326) then
-            cameraVelocity.ry = cameraVelocity.ry - 5.0
+            cameraVelocity.ry = -5.0
         elseif (key == 327) then
-            cameraVelocity.rz = cameraVelocity.rz + 5.0
+            cameraVelocity.rz = 5.0
         elseif (key == 329) then
-            cameraVelocity.rz = cameraVelocity.rz - 5.0
+            cameraVelocity.rz = -5.0
         elseif (key == 320) then
             if gravityEnabled then
                 gravityEnabled = false
@@ -48,25 +52,25 @@ function MoveCamera(action, key)
         end
     elseif (action == "Up") then
         if (key == 87) then
-            cameraVelocity.z = cameraVelocity.z + 10.0
+            cameraVelocity.z = 0
         elseif (key == string.byte("S")) then
-            cameraVelocity.z = cameraVelocity.z - 10.0
+            cameraVelocity.z = 0
         elseif (key == 65) then
-            cameraVelocity.x = cameraVelocity.x + 10.0
+            cameraVelocity.x = 0
         elseif (key == string.byte("D")) then
-            cameraVelocity.x = cameraVelocity.x - 10.0
+            cameraVelocity.x = 0
         elseif (key == 328) then
-            cameraVelocity.rx = cameraVelocity.rx - 5.0
+            cameraVelocity.rx = 0
         elseif (key == 322) then
-            cameraVelocity.rx = cameraVelocity.rx + 5.0
+            cameraVelocity.rx = 0
         elseif (key == 324) then
-            cameraVelocity.ry = cameraVelocity.ry - 5.0
+            cameraVelocity.ry = 0
         elseif (key == 326) then
-            cameraVelocity.ry = cameraVelocity.ry + 5.0
+            cameraVelocity.ry = 0
         elseif (key == 327) then
-            cameraVelocity.rz = cameraVelocity.rz - 5.0
+            cameraVelocity.rz = 0
         elseif (key == 329) then
-            cameraVelocity.rz = cameraVelocity.rz + 5.0
+            cameraVelocity.rz = 0
         elseif (key == 320) then
         end
     end
@@ -82,7 +86,6 @@ end
 
 function MouseUpdate(delta)
 	camera = Transform.Get(10)
-	cam = camera:get_rotation()
 	cameraAn = {x = cameraVelocity.x, y = cameraVelocity.y, z = cameraVelocity.z,
 		rx = -cameraR.y, ry = -cameraR.x, rz = 0}
 	cameraR.x = 0
